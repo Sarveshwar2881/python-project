e8daf2ec5cee5178c9901354df67d5f34d4e1f55
Sarveshwar <sudeshlehri429@gmail.com>
2025-05-22 17:58:24 +0530

    22/5/25

--- Chapter6(Fuction & Recursion).py
+++ Chapter6(Fuction & Recursion).py
@@ -38,18 +38,64 @@
 # Modulo=calc_Modulo(144,20)
 # print(Modulo)
 
-# for average
-def calc_avg(a,b,c,d,e):
+# # for Average
+# def calc_avg(a,b,c,d,e):    
+#     sum=a+b+c+d+e
+#     avg= sum/5
+#     print(avg)
+#     return avg
+
+# asd=calc_avg(45,89,67,77,85)
+
+# print("apnacollage")
+# # for space in between 2 stings as like ("string","string")
+# print("sarvesh","kumar")
+# print multipul parmeters in 1 line.
+# print("apnacollage", end=" ") 
+# print("sarvesh")
+# print("kumar")
+
+# print ("This is a bug but developer is not understand tnis.")
+# print ("This is a bug but developer is not understand tnis.")
+
+# What is default parameters?
+
+# write a fuction to print the length of list? (List is the parameter)
+# nums=[1,2,3,4,5,6,7,8,9,10,5,4,4564,5,4,89,99,54,67]
+# def print_len():
+#     print(len(nums))
+# print_len()
     
-    sum=a+b+c+d+e
-    avg= sum/5
-    #print(avg)
-    return avg
+# Heros=["tiger","rakhi","mohit","batman","hitman"]
+# def heros_name():
+#     print(len(Heros))
+# heros_name()
 
-asd=calc_avg(45,89,67,77,85)
+# nums=(1,2,3,4,5,6,7,8,9,10,5,4,4564,5,4,89,99,54,67,)
+# Heros={"tiger":"rakhi","mohit":"batman","hitman":12345}
+# print(*nums,*Heros,end=" ")
 
+# nums = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 4564, 5, 4, 89, 99, 54, 67)
+# Heros = ["tiger","rakhi", "mohit", "batman", "hitman"]
+# print(*nums, *Heros, end=" ")
 
+# nums = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 4564, 5, 4, 89, 99, 54, 67)
+# Heros = ["tiger","rakhi", "mohit", "batman", "hitman"]
 
+# def length_of_list(value):
+#    print(len(value))    
 
+# length_of_list(Heros)
+# length_of_list(nums)                                                                      
 
+# how to print any one value form list and tuple and string and dictionary and sets.
+# nums = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 4564, 5, 4, 89, 99, 54, 67)
+# Heros = ["tiger","rakhi", "mohit", "batman", "hitman"]
+# dict={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 4564, 5}
+# value={1:2, 3:4, 5:6, 7:8, 9:10, 5:4, 45:5}
 
+# dict_list = list(dict)
+# value_list = list(value)
+# print(nums[12],Heros[3],dict_list[9],value_list[4], end= " ")
+
+#  if any sign is appearing in list of printed list then print in last with parnthahis.print()
