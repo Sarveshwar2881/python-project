3f2519a3aceb1579aa0cb73c239991c4a46daa76
Sarveshwar <sudeshlehri429@gmail.com>
2025-06-16 11:10:42 +0530

    16/6/2025

---
+++ Learn Python/FirstProgram.py
@@ -0,0 +1,207 @@
+str1 ="sa paragraph is a distinct section of writing, typically consisting of several sentences "
+str2 = "that focuses on a single idea or topic, helping to organize and structure written" 
+ends = str1.endswith("tences")
+capi = str2.capitalize()
+print(len1) 
+print(len2) 
+# # str1 ="Sarveshwar Kumar"
+# name=input("What is your name--- ")
+# print("your name"+name)
+# str1 = "sarveshwar" # add 2 strings
+# str2 = "kumar"
+# addall = str1+str2
+# print(addall)
+# str1 = "sarveshwar" # add 2 strings with space
+# str2 = "kumar" 
+# print(str1,str2)
+# print(str1+" "+str2)  #both way.
+# str1="this is my old school." #how to calcuate length of string and how to add in other string.
+# print(len(str1))
+# str2="this is my favorite new school."
+# print(len(str2))
+# addall= str1+str2
+# print(len(addall))
+# str="SARVESHWAR KUMAR" # how to print character?
+# print(str[5])
+# print(str[8])
+# str1="SARVESHWAR KUMAR" # how to add variable and print character?c
+# ch1 = str1[5] 
+# ch2 = str1[8] 
+# ch3 = str1[12]
+# print(ch1)
+# print(ch3)
+# print(ch2)
+# print(ch1,ch2,ch3)
+# Slicing str[starting_idx:ending_idx] beehc ka part nekal ne ke liye (important.)
+# str="SARVESHWAR KUMAR"
+# print(str[2:8]) # print hoga == RVESHW
+# str="SARVESHWAR KUMAR"
+# print(str[0:]) # ending index tak print hoga --SARVESHWAR KUMAR
+# str="SARVESHWAR KUMAR"
+# print(str[:]) # full string print hoga 
+# str="SARVESHWAR KUMAR"
+# print(str[:]) 
+#when you want to go last index.(2nd way)
+# str="A while back I needed to count the amount of letters that a piece of text in an email template had"
+# print(str[0:len(str)])
+
+#Negative Index (...-6-5-4-3-2-1)
+# str="Sarveshwar Kumar Sharma"
+# print(str[-20:-6])
+
+#string function
+# endwith = print(str.endswithstr("word or character"))
+# capitalize = print(str.capitalize(word or character))
+# replac = print(str.replace("new value", "old value")) (word or character)
+# find = print(str.find(word or character))
+# count = print(str.count("pheragraph")) string
+# name = input("Enter Your Name:")
+# print("length of your name:",len(name))
+age=18
+if(age>=18):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+    print("he can apply for vote and licence")
+    print("He is > and = to 18.")
+# age=18
+# if(age>=18):
+#     print("he can apply for vote and licence")
+#     print("He is > and = to 18.")
+# example
+#trafficlight=input("Enter Traffic Color:-")
+#=input("enter traffic colour -- :")
+# if(trafficlight=="red" or trafficlight=='RED'):
+#     print("stop")
+# if(trafficlight=="Green"):
+#     print("Go and Run.")
+# if(trafficlight=="Blinking Yellow"):
+#     print("wait and watch then run.")
+# elif(trafficlight=="Yellow"):
+#     print("wait and watch.")
+# else:
+#     print("Wait and watch then go slow- slow.")
+
+# 
+# time = float(input("Write Your office entered time:-"))
+# if (time>=9.30 and time<=10.00):
+#     print("Welcome")
+# # elif(time>=9.31):
+# #     print("Go to Home and Rest")
+# else:
+#     print("Time out, Go to Home and Rest")
+# age1=38
+# age2=38
+# print(not(age1>=age2))
+# str = "sarveshwar" 
+# print(str.capitalize())
+
+#Conditional Statement
+#marks=int(input("Enter Student Marks:-"))
+# if (marks>=90):
+#     print("Grade=A")
+# elif (marks<90 and marks>80):
+#     print("Grade=B")
+# elif (marks<80 and marks>70):
+#     print("Grade=C")
+# elif (marks<70 and marks>=40): 
+#     print("Grade=D")
+# else:
+#     print ("Fail")
+# or
+# marks=int(input("Enter Student Marks:-"))
+# if (marks>=90):
+#     Grade="A"
+# elif (marks<90 and marks>80):
+#     Grade="B"
+# elif (marks<80 and marks>70):
+#     Grade="C"
+# elif (marks<70 and marks>=40): 
+#     Grade="D"
+#     print("Result of student := "+Grade)
+# else:
+#     print("Fail") 
+# age = 66
+# if(age>=18):
+#     if(age>=80):    
+#         print("can not drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("Can not Drive.")
+# TEST PROGRAM 1st Chapter-2
+# int1 = 444444
+# if(int1%2==0):
+#     print("Even")
+# else:
+#     print("Odd")
+# TEST PROGRAM 2nd Chapter-2 (for check biggest number.)
+# a=55
+# b=55
+# c=100
+# if(a>=b and a>=c):
+#     print("First big number:-", a)
+# elif(b>=c):
+#     print("Second big number:-",b)
+# else:
+#     print("Last is biggest:-" ,c)
+# TEST PROGRAM 3nd Chapter-2 (for check divied by 7 or not.)
+# a=46461231460
+# if (a%7==0):
+#     print(True)
+# else:
+#     print(False)
+# a=7323
+# b=7323
+# if(a!=b):
+#     print("false")
+# else:
+#     print("true")
+# a = 995
+# if (a%2==1):
+#     print("Weird")
+# how to get input form user?
+# num1 = int(input("Enter first number:- "))
+# num2 = int(input("Enter second number:- "))
+# num3 = int(input("Enter Third number:- "))
+# num4 = int(input("Enter Fourth number:- "))
+# print(num1+num2+num3+num4)
+# marks=[96.4,45.9,88.6,92.8,41.5,56.6] 
+# print(marks)
+# print(type(marks))
+# Print hoga
+# Python/FirstProgram.py
+# [96.4, 45.9, 88.6, 92.8, 41.5, 56.6]
+# <class 'list'>
+# PS C:\Users\Sarveshwar\Desktop\Python>
+# marks=[96.4,45.9,88.6,92.8,41.5,56.6,96.4,45.9,88.6,92.8,41.5,56.6] 
+# print(marks[:])
+# print(len(marks))
+# list=[40,60,35,15,66,88] # list append
+# list.extend([71,99,10202,1.2]) # None return hoga
+# print(list.sort())
+# print(list)
+# print(list.sort(reverse=True)) # None return hoga
+# print(list)
+# print(list.sort(reverse=False)) # None return hoga
+# print(list)
+# list=["mango", "Banana","papya","Apple","Lichi","Guava"] # list append
+# list.extend(["Grapes","orange"]) # None return hoga
+# print(list.reverse())
+# print(list)
+# print(list.sort(reverse=True)) # None return hoga
+# print(list)
+# print(list.sort(reverse=False)) # None return hoga
+# print(list)
+
+# List Insert
+# a=[1,4,2,5]
+# a.insert(2,6)
+# print(a) print hoga = [1, 4, 6, 2, 5]
+
+#list remove
+# list= [6,7,3,5,19]
+# list.remove(19)
+# print(list) 
+#print hoga # = [6, 7, 3, 5]
+
+
+
+# print("hi this is my new code for vs code ")
